d536d27b160781d997eb7277d9dfb674a69d2787
matheeshapathirana <pathiranamatheesha@gmail.com>
2025-05-01 13:47:40 +0530

    Add loginView form and associated Java class for user authentication

---
+++ src/main/java/View/loginView.java
@@ -0,0 +1,19 @@
+package View;
+
+import javax.swing.*;
+
+public class loginView {
+    private JTabbedPane tabs;
+    private JFormattedTextField txtemail;
+    private JPasswordField txtpassword;
+    private JButton btnlogin;
+    private JPasswordField passwordField2;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton btnregister;
+    private JLabel lblloginemail;
+    private JLabel lblloginpassword;
+    private JLabel lblregisterusername;
+    private JLabel lblregisteremail;
+    private JLabel lblregisterpassword;
+}
