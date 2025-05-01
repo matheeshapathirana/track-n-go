8d4f74d986b86126a39313ed69b873e646b3d35f
matheeshapathirana <pathiranamatheesha@gmail.com>
2025-05-01 19:25:37 +0530

    Add Personnel Management UI components to AdminView

--- src/main/java/View/adminView.java
+++ src/main/java/View/adminView.java
@@ -15,4 +15,23 @@ public class adminView {
     private JLabel lbldeliverytime;
     private JLabel lbldelays;
     private JPanel trackbackpanel;
+    private JPanel PersonnelManagerPanel;
+    private JLabel IDlbl;
+    private JLabel Namelbl;
+    private JLabel contactlbl;
+    private JLabel schedulelbl;
+    private JLabel AssignedRoutelbl;
+    private JTextField txtName;
+    private JTextField txtContact;
+    private JTextField txtSchedule;
+    private JTextField txtRoute;
+    private JButton addDriverButton;
+    private JButton updateDriverButton;
+    private JButton deleteDriverButton;
+    private JTextField txtID;
+    private JTextArea txtAreaHistory;
+    private JTable AllDriversView;
+    private JLabel alllDriverslbl;
+    private JPanel spacer3;
+    private JPanel spacer2;
 }
