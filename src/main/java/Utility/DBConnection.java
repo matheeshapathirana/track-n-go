7d0c5a8bf118bfe6138d1a603dfb984330a73af7
themiya22 <themiyasilva22@gmail.com>
2025-05-01 19:14:59 +0530

    Added a shared DBConnection and enhanced the UI with JScrollPane for JTable for better UX

--- src/main/java/Utility/DBConnection.java
+++ src/main/java/Utility/DBConnection.java
@@ -8,18 +8,14 @@ public class DBConnection {
     private static final String URL = "jdbc:mysql://pixel-host.zapto.org:3306/trackngo";
     private static final String USER = "user";
     private static final String PASSWORD = "user2006";
-    private static Connection connection = null;
 
     public static Connection getConnection() {
-        if (connection == null) {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                System.out.println("Database connected.");
-            } catch (Exception e) {
-                System.out.println("Database connection error: " + e.getMessage());
-            }
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD); // always fresh
+        } catch (Exception e) {
+            System.out.println("Database connection error: " + e.getMessage());
+            return null;
         }
-        return connection;
     }
 }
