c16b79c664635881d5e0d216e55422c99fdb66ce
themiya22 <themiyasilva22@gmail.com>
2025-05-01 18:25:56 +0530

    Added delivery personnel management feature (Update and Delete)

--- src/main/java/Controller/DeliveryPersonnelController.java
+++ src/main/java/Controller/DeliveryPersonnelController.java
@@ -3,20 +3,19 @@ package Controller;
 import Model.DeliveryPersonnel;
 import Model.DeliveryPersonnelDAO;
 
-public class DeliveryPersonnelController   {
+public class DeliveryPersonnelController {
     private DeliveryPersonnelDAO DAO = new DeliveryPersonnelDAO();
-
     //to add the personal controller using the code used in DAO package
-    public void AddDeliveryPersonnel(DeliveryPersonnel p) {
+    public void addDeliveryPersonnel(DeliveryPersonnel p) {
         DAO.addPersonnel(p);
     }
     //to update personnel
-    public void DeleteDeliveryPersonnel(DeliveryPersonnel p) {
-        DAO.deletePersonnel(p.getPersonnelID());
-    }
-    //to delete personnel
-    public void UpdateDeliveryPersonnel(DeliveryPersonnel p) {
+    public void updateDeliveryPersonnel(DeliveryPersonnel p) {
         DAO.updatePersonnel(p);
     }
-}
+    //to delete personnel
+    public void deleteDeliveryPersonnel(DeliveryPersonnel p) {
+        DAO.deletePersonnel(p.getPersonnelID());
+    }
 
+}
