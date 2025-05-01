f3be7b0fd4877eb7c6815e0b8da68fbd87635ee2
matheeshapathirana <pathiranamatheesha@gmail.com>
2025-05-01 19:05:21 +0530

    Add AdminView UI with tabbed panels for tracking

---
+++ src/main/java/View/adminView.java
@@ -0,0 +1,18 @@
+package View;
+
+import javax.swing.*;
+
+public class adminView {
+    private JTabbedPane tabbedPane1;
+    private JTextField txtshipmentid;
+    private JTextField txtlocation;
+    private JTextField txtdeliverytimes;
+    private JTextField txtdelays;
+    private JButton btnupdatetrack;
+    private JTable tracktable;
+    private JLabel lblshipmentid;
+    private JLabel lbllocation;
+    private JLabel lbldeliverytime;
+    private JLabel lbldelays;
+    private JPanel trackbackpanel;
+}
