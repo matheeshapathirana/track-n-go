9c4e3c03be4f153cedb77e6df0fc223bf3f876ef
matheeshapathirana <pathiranamatheesha@gmail.com>
2025-05-04 11:06:50 +0530

    Add driverView form and implementation for driver role management

---
+++ src/main/java/View/driverView.java
@@ -0,0 +1,20 @@
+package View;
+
+import javax.swing.*;
+
+public class driverView {
+    private JPanel trackbackpanel;
+    private JLabel lblshipmentid;
+    private JTextField txtshipmentid;
+    private JLabel lbllocation;
+    private JTextField txtlocation;
+    private JLabel lbldeliverytime;
+    private JTextField txtdeliverytimes;
+    private JLabel lbldelays;
+    private JTextField txtdelays;
+    private JButton btnupdatetrack;
+    private JTable tracktable;
+    private JComboBox comboBox1;
+    private JTextField txttrackingid;
+    private JLabel lbltrackingid;
+}
